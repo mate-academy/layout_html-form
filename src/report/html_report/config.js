8959cb6e0891fcf39551cc8e1d80f1d4bdaab71d
Taisiiapn <taisiia.pomazan@gmail.com>
2019-05-29 14:23:13 +0300

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,253 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_entire-document_0_document_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190529-141215\\test_entire-document_0_document_0_tablet_h.png",
+        "selector": "document",
+        "fileName": "test_entire-document_0_document_0_tablet_h.png",
+        "label": "entire-document",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/html-form/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -8
+          },
+          "misMatchPercentage": "6.05",
+          "analysisTime": 160
+        },
+        "diffImage": "..\\bitmaps_test\\20190529-141215\\failed_diff_test_entire-document_0_document_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_form_0_form_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190529-141215\\test_form_0_form_0_tablet_h.png",
+        "selector": "form",
+        "fileName": "test_form_0_form_0_tablet_h.png",
+        "label": "form",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/html-form/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -40
+          },
+          "misMatchPercentage": "6.15",
+          "analysisTime": 321
+        },
+        "diffImage": "..\\bitmaps_test\\20190529-141215\\failed_diff_test_form_0_form_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_fieldset_0_fieldset_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190529-141215\\test_fieldset_0_fieldset_0_tablet_h.png",
+        "selector": "fieldset",
+        "fileName": "test_fieldset_0_fieldset_0_tablet_h.png",
+        "label": "fieldset",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/html-form/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -10
+          },
+          "misMatchPercentage": "1.03",
+          "analysisTime": 169
+        },
+        "diffImage": "..\\bitmaps_test\\20190529-141215\\failed_diff_test_fieldset_0_fieldset_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_fieldset_1_fieldset__n1_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190529-141215\\test_fieldset_1_fieldset__n1_0_tablet_h.png",
+        "selector": "fieldset.__n1",
+        "fileName": "test_fieldset_1_fieldset__n1_0_tablet_h.png",
+        "label": "fieldset",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/html-form/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -10
+          },
+          "misMatchPercentage": "2.06",
+          "analysisTime": 180
+        },
+        "diffImage": "..\\bitmaps_test\\20190529-141215\\failed_diff_test_fieldset_1_fieldset__n1_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_fieldset_2_fieldset__n2_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190529-141215\\test_fieldset_2_fieldset__n2_0_tablet_h.png",
+        "selector": "fieldset.__n2",
+        "fileName": "test_fieldset_2_fieldset__n2_0_tablet_h.png",
+        "label": "fieldset",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/html-form/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 10
+          },
+          "misMatchPercentage": "0.83",
+          "analysisTime": 273
+        },
+        "diffImage": "..\\bitmaps_test\\20190529-141215\\failed_diff_test_fieldset_2_fieldset__n2_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_fieldset_3_fieldset__n3_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190529-141215\\test_fieldset_3_fieldset__n3_0_tablet_h.png",
+        "selector": "fieldset.__n3",
+        "fileName": "test_fieldset_3_fieldset__n3_0_tablet_h.png",
+        "label": "fieldset",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/html-form/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -10
+          },
+          "misMatchPercentage": "1.75",
+          "analysisTime": 103
+        },
+        "diffImage": "..\\bitmaps_test\\20190529-141215\\failed_diff_test_fieldset_3_fieldset__n3_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_submit-button_0_typesubmit_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190529-141215\\test_submit-button_0_typesubmit_0_tablet_h.png",
+        "selector": "[type=\"submit\"]",
+        "fileName": "test_submit-button_0_typesubmit_0_tablet_h.png",
+        "label": "submit-button",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/html-form/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_field-placeholder_0_data-qafield-placeholder_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190529-141215\\test_field-placeholder_0_data-qafield-placeholder_0_tablet_h.png",
+        "selector": "[data-qa=\"field-placeholder\"]",
+        "fileName": "test_field-placeholder_0_data-qafield-placeholder_0_tablet_h.png",
+        "label": "field-placeholder",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/html-form/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -168,
+            "height": -245
+          },
+          "misMatchPercentage": "4.01",
+          "analysisTime": 187
+        },
+        "diffImage": "..\\bitmaps_test\\20190529-141215\\failed_diff_test_field-placeholder_0_data-qafield-placeholder_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_field-placeholder_0_data-qafield-value_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190529-141215\\test_field-placeholder_0_data-qafield-value_0_tablet_h.png",
+        "selector": "[data-qa=\"field-value\"]",
+        "fileName": "test_field-placeholder_0_data-qafield-value_0_tablet_h.png",
+        "label": "field-placeholder",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/html-form/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_field-placeholder_1_data-qafield-value__n1_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190529-141215\\test_field-placeholder_1_data-qafield-value__n1_0_tablet_h.png",
+        "selector": "[data-qa=\"field-value\"].__n1",
+        "fileName": "test_field-placeholder_1_data-qafield-value__n1_0_tablet_h.png",
+        "label": "field-placeholder",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/html-form/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "error": "Reference file not found C:\\project6\\layout_html-form\\backstop_data\\bitmaps_reference\\test_field-placeholder_1_data-qafield-value__n1_0_tablet_h.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "test"
+});
