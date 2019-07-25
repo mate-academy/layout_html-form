f3b237ea5a00d7c5a4c50ae82976e96fd3eb0ca4
ivan_safiulin <ivansafiulin@myrambler.ru>
2019-07-25 21:24:56 +0300

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,168 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_entire-document_0_document_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190725-212325\\test_entire-document_0_document_0_tablet_h.png",
+        "selector": "document",
+        "fileName": "test_entire-document_0_document_0_tablet_h.png",
+        "label": "entire-document",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/html-form/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -792,
+            "height": -588
+          },
+          "misMatchPercentage": "5.29",
+          "analysisTime": 55
+        },
+        "diffImage": "..\\bitmaps_test\\20190725-212325\\failed_diff_test_entire-document_0_document_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_form_0_form_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190725-212325\\test_form_0_form_0_tablet_h.png",
+        "selector": "form",
+        "fileName": "test_form_0_form_0_tablet_h.png",
+        "label": "form",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/html-form/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -776,
+            "height": -540
+          },
+          "misMatchPercentage": "5.74",
+          "analysisTime": 41
+        },
+        "diffImage": "..\\bitmaps_test\\20190725-212325\\failed_diff_test_form_0_form_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_fieldset_0_fieldset_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190725-212325\\test_fieldset_0_fieldset_0_tablet_h.png",
+        "selector": "fieldset",
+        "fileName": "test_fieldset_0_fieldset_0_tablet_h.png",
+        "label": "fieldset",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/html-form/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -772,
+            "height": -3
+          },
+          "misMatchPercentage": "22.65",
+          "analysisTime": 33
+        },
+        "diffImage": "..\\bitmaps_test\\20190725-212325\\failed_diff_test_fieldset_0_fieldset_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_submit-button_0_typesubmit_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190725-212325\\test_submit-button_0_typesubmit_0_tablet_h.png",
+        "selector": "[type=\"submit\"]",
+        "fileName": "test_submit-button_0_typesubmit_0_tablet_h.png",
+        "label": "submit-button",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/html-form/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -174,
+            "height": -159
+          },
+          "misMatchPercentage": "2.92",
+          "analysisTime": 13
+        },
+        "diffImage": "..\\bitmaps_test\\20190725-212325\\failed_diff_test_submit-button_0_typesubmit_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_field-placeholder_0_data-qafield-placeholder_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190725-212325\\test_field-placeholder_0_data-qafield-placeholder_0_tablet_h.png",
+        "selector": "[data-qa=\"field-placeholder\"]",
+        "fileName": "test_field-placeholder_0_data-qafield-placeholder_0_tablet_h.png",
+        "label": "field-placeholder",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/html-form/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -59,
+            "height": -159
+          },
+          "misMatchPercentage": "8.70",
+          "analysisTime": 23
+        },
+        "diffImage": "..\\bitmaps_test\\20190725-212325\\failed_diff_test_field-placeholder_0_data-qafield-placeholder_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\test_field-placeholder_0_data-qafield-value_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190725-212325\\test_field-placeholder_0_data-qafield-value_0_tablet_h.png",
+        "selector": "[data-qa=\"field-value\"]",
+        "fileName": "test_field-placeholder_0_data-qafield-value_0_tablet_h.png",
+        "label": "field-placeholder",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/html-form/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -163,
+            "height": -159
+          },
+          "misMatchPercentage": "3.47",
+          "analysisTime": 19
+        },
+        "diffImage": "..\\bitmaps_test\\20190725-212325\\failed_diff_test_field-placeholder_0_data-qafield-value_0_tablet_h.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "test"
+});
