f36720fbf6e21a451ca190a7020e43bbd6751914
Andrii Torhashov <torgashov19q@gmail.com>
2023-02-09 21:06:53 +0200

    090223

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -183,7 +183,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63048" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53880" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
